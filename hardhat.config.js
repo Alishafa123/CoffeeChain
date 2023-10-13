6db34c683937014971d2befbd1b041ee5fbf8b0c
Ali Shafa <116679960+Alishafa123@users.noreply.github.com>
2023-10-14 02:05:03 +0500

    deployed contract on sepolia test network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const ALCHEMY_API_KEY = "9q3Dg267mEPzLl6vtU8NzDDSEgRT3PSv";
+const SEPOLIA_PRIVATE_KEY = "b2b8f780b81a1c95275416974017f7525486fce63d681ae7d1a6ea2c78194ef0";
+
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY],
+    },
+  },
 };
