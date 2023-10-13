7435025d332d25cd41dbe880d89e10de84b1dae6
Ali Shafa <116679960+Alishafa123@users.noreply.github.com>
2023-10-13 23:33:01 +0500

    contract deploy on hardhat blockchain

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,22 +7,13 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+ 
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  const chai = await hre.ethers.deployContract("chai");
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  await chai.waitForDeployment();
 
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+ console.log(await chai.getAddress());
 }
 
 // We recommend this pattern to be able to use async/await everywhere
